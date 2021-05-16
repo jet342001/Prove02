a53366f1d7c32c77c6cc54c00d8f325bbf27d929
Stephen Gee <jet342001@yahoo.com>
2021-05-15 21:57:07 -0600

    heroku fix

--- app.js
+++ app.js
@@ -9,23 +9,25 @@ const User = require("./model/user");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const cors = require('cors') // Place this with other requires (like 'path' and 'express')
-...
+const cors = require("cors"); // Place this with other requires (like 'path' and 'express')
+
 const corsOptions = {
-    origin: "https://<your_app_name>.herokuapp.com/",
-    optionsSuccessStatus: 200
+  origin: "https://<your_app_name>.herokuapp.com/",
+  optionsSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 
 const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4,
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+const MONGODB_URL =
+  process.env.MONGODB_URL ||
+  "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
 
 const app = express();
 
@@ -54,9 +56,7 @@ app.use((req, res, next) => {
 });
 
 mongoose
-  .connect(
-    MONGODB_URL, options
-  )
+  .connect(MONGODB_URL, options)
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
