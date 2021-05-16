ecaf9b2e01f203bdc8697fb60e40fa03c2bf53ee
Stephen Gee <jet342001@yahoo.com>
2021-05-15 21:52:07 -0600

    heroku update

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const path = require("path");
 const mongoose = require("mongoose");
 
-const port = "5000"; //process.env.PORT ||
+const port = "process.env.PORT || 5000 ";
 
 const http = require("http");
 const User = require("./model/user");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+...
+const corsOptions = {
+    origin: "https://<your_app_name>.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
 
 const app = express();
 
@@ -37,7 +55,7 @@ app.use((req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://Steve:M2j9DCBGynRG7Clu@cluster0.u8ta3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGODB_URL, options
   )
   .then((result) => {
     User.findOne().then((user) => {
