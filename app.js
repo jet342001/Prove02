83fd3f8ae01f18b37843df3f5f455c6ce0397d50
Stephen Gee <jet342001@yahoo.com>
2021-05-18 10:17:35 -0600

    test2

--- app.js
+++ app.js
@@ -25,9 +25,8 @@ const options = {
   family: 4,
 };
 
-const MONGODB_URL =
-  process.env.MONGODB_URL ||
-  "mongodb+srv://test:Fx7Bc9NYqesj7ffp@cluster0.u8ta3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGODB_URL = process.env.MONGODB_URL; //||
+//"mongodb+srv://test:Fx7Bc9NYqesj7ffp@cluster0.u8ta3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const booksData = require("./routes/add-book");
 
